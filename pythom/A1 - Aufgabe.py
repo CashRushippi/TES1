803387e6afd6832484f1294561be3008881a0994
Kastri <kastri.rushiti11@gmail.com>
2025-06-24 19:46:23 +0200

    pythom ordner erstellt mit de ufgabe

---
+++ pythom/A1 - Aufgabe.py
@@ -0,0 +1,10 @@
+# 5 in Variable v_espresso speichern
+
+# 10 in Variable v_machiatto
+
+# v_machiatto mit v_espresso multiplizieren und in Variable v_ristretto speichern
+
+#v_ristretto minus 5 rechnen und in Variable v_lungo speichern
+
+#Produkt der Variablen v_machiatto und v_lungo in der Konsole ausgeben
+
---
+++ pythom/A1.py
@@ -0,0 +1,11 @@
+# 5 in Variable v_espresso speichern
+v_espresso = 5
+# 10 in Variable v_machiatto
+v_machiatto = 10
+# v_machiatto mit v_espresso multiplizieren und in Variable v_ristretto speichern
+v_ristretto = v_machiatto * v_espresso
+#v_ristretto minus 5 rechnen und in Variable v_lungo speichern
+v_lungo = v_ristretto - 5
+#Produkt der Variablen v_machiatto und v_lungo in der Konsole ausgeben
+
+print(v_machiatto * v_lungo)
